bb208ff02af523d4275b5bbfd5b385f4fe3e5512
Piers14 <61315972+Piers14@users.noreply.github.com>
2021-01-23 23:00:11 +0000

    Update data_loader.h

--- data_loader.h
+++ data_loader.h
@@ -5,9 +5,12 @@ class data_loader
 {
 private:
 	int batch_size;
-	matrix<double> data;
-	std::string file_path;
 	double valid_pct;
+
+	matrix<double> train_data;
+	matrix<double> val_data;
+	std::string file_path;
+	
 
 public:
 	data_loader(std::string _file_path, int _batch_size = 16, double valid_pct = 0.2);
